1114b94861c8aad84d23ac9d9524e3d72ec2ed4d
QYIOPLMGJFDGH <Uttamk1578@gmail.com>
2024-12-15 17:14:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,8 @@ from pyrogram.errors import MessageIdInvalid, ChatAdminRequired, EmoticonInvalid
 from random import choice
 from bot import TelegramBot, logger
 from bot.config import Telegram
-from bot.modules.static import *
 from logging import getLogger
-from logging.config import dictConfig
 from os import environ as env
-
-
 
 LOGGER_CONFIG_JSON = {
     'version': 1,
@@ -41,35 +37,6 @@ LOGGER_CONFIG_JSON = {
         }
     }
 }
-# Logger Configuration
-dictConfig(LOGGER_CONFIG_JSON)
-
-# Emojis Text
-SupportedEmojisText = """
-**I currently support following emojis:**
-""" + '\n'.join(' '.join(Telegram.EMOJIS[i:i+5]) for i in range(0, len(Telegram.EMOJIS), 5))
-
-# Command Handlers
-@TelegramBot.on_message(filters.command('start') & (filters.private | filters.group))
-async def start_command(_, msg: Message):
-    return await msg.reply(
-        text=WelcomeText % {'first_name': msg.from_user.first_name if msg.from_user else 'Anonymous'},
-        quote=True,
-        reply_markup=InlineKeyboardMarkup([
-            [InlineKeyboardButton(text='Add me to chat', url=f'https://t.me/{Telegram.BOT_USERNAME}?startgroup=botstart')],
-            [InlineKeyboardButton(text='Source Code', url='https://github.com/TheCaduceus/TG-ReactionBot')]
-        ])
-    )
-
-@TelegramBot.on_message(filters.command('emojis') & (filters.private | filters.group))
-async def send_emojis(_, msg: Message):
-    return await msg.reply(
-        text=SupportedEmojisText,
-        quote=True,
-        reply_markup=InlineKeyboardMarkup([
-            [InlineKeyboardButton(text='Reference', url='https://github.com/TheCaduceus/TG-ReactionBot/blob/main/bot/config.py#L8')]
-        ])
-    )
 
 @TelegramBot.on_message(filters.all)
 async def send_reaction(_, msg: Message):
@@ -101,8 +68,7 @@ TelegramBot = Client(
     name="bot",
     api_id=Telegram.API_ID,
     api_hash=Telegram.API_HASH,
-    bot_token=Telegram.BOT_TOKEN,
-    plugins={'root': 'bot/plugins'}
+    bot_token=Telegram.BOT_TOKEN
 )
 
 if __name__ == '__main__':
